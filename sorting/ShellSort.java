7a2f5e8d3ef5e04634e14ea36c40ce211cc2742f
shadowwingz <shadowwingzhang@gmail.com>
2019-02-13 23:55:32 +0800

    [Add] ShellSort

---
+++ sorting/ShellSort.java
@@ -0,0 +1,35 @@
+package sorting;
+
+/**
+ * 希尔排序
+ * <p>
+ * Created by shadowwingz on 2019-02-13 21:32
+ */
+public class ShellSort extends BaseSort {
+
+    @Override
+    public void sort(Comparable[] array) {
+        int N = array.length;
+        int h = 1;
+        while (h < N / 3) {
+            h = 3 * h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(array[j], array[j - h]); j -= h) {
+                    exchange(array, j, j - h);
+                }
+            }
+            h = h / 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("希尔排序\n");
+        ShellSort shellSort = new ShellSort();
+        Comparable[] array = {3, 4, 7, 2, 6, 5, 1, 8, 9};
+        printOriginArray(array);
+        shellSort.sort(array);
+        printSortedArray(array);
+    }
+}
